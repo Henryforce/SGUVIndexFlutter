06fb99c9d6de1f4dfdd3ec00c3d8d52d29384767
Henry Serrano <hj_serrano@hotmail.com>
2021-07-11 08:51:24 +0900

    Formatted Data Classes

--- lib/modules/main/data/uv_data.dart
+++ lib/modules/main/data/uv_data.dart
@@ -5,13 +5,8 @@ class UVData extends Equatable {
 
   factory UVData.fromJSON(Map<String, dynamic> json) {
     final items = json['items'] as List;
-    final uvItems = items.map(
-      (item) => UVItem.fromJsonMap(item)
-    ).toList();
-    return UVData(
-      items: uvItems, 
-      apiInfo: APIInfo.fromJSON(json['api_info'])
-    );
+    final uvItems = items.map((item) => UVItem.fromJsonMap(item)).toList();
+    return UVData(items: uvItems, apiInfo: APIInfo.fromJSON(json['api_info']));
   }
 
   final List<UVItem> items;
@@ -24,8 +19,7 @@ class UVData extends Equatable {
 class APIInfo extends Equatable {
   const APIInfo({required this.status});
 
-  APIInfo.fromJSON(Map<String, dynamic> json)
-    : status = json['status'];
+  APIInfo.fromJSON(Map<String, dynamic> json) : status = json['status'];
 
   final String status;
 
@@ -34,22 +28,19 @@ class APIInfo extends Equatable {
 }
 
 class UVItem extends Equatable {
-  const UVItem({
-    required this.timestamp, 
-    required this.updateTimestamp, 
-    required this.records
-  });
+  const UVItem(
+      {required this.timestamp,
+      required this.updateTimestamp,
+      required this.records});
 
   factory UVItem.fromJsonMap(Map<String, dynamic> json) {
     final records = json['index'] as List;
-    final uvDataRecords = records.map(
-      (record) => UVDataRecord.fromJson(record)
-    ).toList();
+    final uvDataRecords =
+        records.map((record) => UVDataRecord.fromJson(record)).toList();
     return UVItem(
-      timestamp: DateTime.parse(json['timestamp']), 
-      updateTimestamp: DateTime.parse(json['update_timestamp']), 
-      records: uvDataRecords
-    );
+        timestamp: DateTime.parse(json['timestamp']),
+        updateTimestamp: DateTime.parse(json['update_timestamp']),
+        records: uvDataRecords);
   }
 
   final DateTime timestamp;
@@ -63,9 +54,9 @@ class UVItem extends Equatable {
 class UVDataRecord extends Equatable {
   const UVDataRecord({required this.value, required this.timestamp});
 
-  UVDataRecord.fromJson(Map<String, dynamic> json) 
-    : value = json['value'],
-      timestamp = DateTime.parse(json['timestamp']);
+  UVDataRecord.fromJson(Map<String, dynamic> json)
+      : value = json['value'],
+        timestamp = DateTime.parse(json['timestamp']);
 
   final int value;
   final DateTime timestamp;
