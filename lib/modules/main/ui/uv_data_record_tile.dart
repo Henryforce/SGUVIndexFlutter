e9f40b1042a5c428f840baf8c0f0fbb2709bfe34
Henryforce <hj_serrano@hotmail.com>
2021-07-11 21:12:00 +0900

    Updated UI (#3)

--- lib/modules/main/ui/uv_data_record_tile.dart
+++ lib/modules/main/ui/uv_data_record_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sguvindex/modules/main/data/uv_ui_data.dart';
 
 class UVDataRecordTile extends StatelessWidget {
@@ -10,10 +11,37 @@ class UVDataRecordTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: ListTile(
-        title: Text(record.uvValue),
-        isThreeLine: true,
-        subtitle: Text(record.date),
-        dense: true,
+        title: _buildTitle(context),
+      ),
+    );
+  }
+
+  Widget _buildTitle(BuildContext context) {
+    return Center(
+      child: Wrap(
+        spacing: 16,
+        crossAxisAlignment: WrapCrossAlignment.center,
+        alignment: WrapAlignment.center,
+        children: [
+          _addText(record.date, alignment: Alignment.centerRight),
+          _addText(record.uvValue, fontSize: 36, fontWeight: FontWeight.bold),
+          _addText(record.uvDescription, alignment: Alignment.centerLeft),
+        ],
+      ),
+    );
+  }
+
+  Widget _addText(String string,
+      {Alignment alignment = Alignment.center,
+      FontWeight fontWeight = FontWeight.normal,
+      double fontSize = 12}) {
+    return Container(
+      width: 80,
+      alignment: alignment,
+      child: Text(
+        string,
+        style:
+            GoogleFonts.montserrat(fontSize: fontSize, fontWeight: fontWeight),
       ),
     );
   }
