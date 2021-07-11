122e7d750ac057f6ff44c52682e973ebc060d79f
Henryforce <hj_serrano@hotmail.com>
2021-07-11 09:12:25 +0900

    Added test.yml for Github Actions Setup (#2)
    * Added test.yml for Github Actions Setup
    
    * Formatted Data Classes
    
    * Updated Flutter Tests
    
    * Disabled custom scripts defined in grind.dart

--- lib/modules/main/data/uv_ui_data.dart
+++ lib/modules/main/data/uv_ui_data.dart
@@ -1,21 +1,14 @@
-
 import 'package:sguvindex/modules/main/data/uv_data.dart';
 
 class UVUIData {
   const UVUIData(
-    {
-      required this.date, 
-      required this.uvValue,
-      required this.uvDescription
-    }
-  );
+      {required this.date, required this.uvValue, required this.uvDescription});
 
   factory UVUIData.fromUVData(UVDataRecord record) {
     return UVUIData(
-      date: record.timestamp.toString(), 
-      uvValue: record.value.toString(), 
-      uvDescription: ''
-    );
+        date: record.timestamp.toString(),
+        uvValue: record.value.toString(),
+        uvDescription: '');
   }
 
   final String date;
